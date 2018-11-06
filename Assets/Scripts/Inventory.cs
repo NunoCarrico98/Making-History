e8fe6425c67ab3bb0fb7efd9aac3a694b7466964
Rui Martins <vandroste@gmail.com>
2018-11-06 15:27:25 +0000

    add Inventory and IPickable .cs files

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<IPickable> inventory = new List<IPickable>();
+}
