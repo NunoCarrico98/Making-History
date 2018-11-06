e2d1ac0b4921dce517e6da24a1ec8240a95c16eb
Nuno Carriço <nuno.carrico.98@gmail.com>
2018-11-06 17:02:31 +0000

    Add Object Detection and Interaction Text

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    //private static CanvasManager instance;
+
+    [SerializeField]
+    private GameObject interactionPanel;
+    [SerializeField]
+    private Text interactionText;
+
+    public static CanvasManager Instance { get; private set; }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        HideInteractionPanel();
+    }
+
+    public void HideInteractionPanel()
+    {
+        interactionPanel.SetActive(false);
+        interactionText.enabled = false;
+    }
+
+    public void ShowInteractionPanel(string text)
+    {
+        interactionText.text = text;
+        interactionPanel.SetActive(true);
+        interactionText.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float maxInteractionDistance;
+
+    private CanvasManager canvasManager;
+    private Camera cam;
+    private RaycastHit raycastHit;
+    private IInteractable currentInteractable;
+
+    private void Start()
+    {
+        canvasManager = CanvasManager.Instance;
+        cam = GetComponentInChildren<Camera>();
+        currentInteractable = null;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckForInteractable();
+        CheckForInteractionClick();
+    }
+
+    private void CheckForInteractionClick()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    private void CheckForInteractable()
+    {
+        if (Physics.Raycast(cam.transform.position,
+            cam.transform.forward, out raycastHit, maxInteractionDistance))
+        {
+            IInteractable newInteractable =
+                raycastHit.collider.GetComponent<IInteractable>();
+
+            if (newInteractable != null && newInteractable.IsInteractable())
+            {
+                SetInteractable(newInteractable);
+            }
+            else
+            {
+                ClearInteractable();
+            }
+
+        }
+        else
+        {
+            ClearInteractable();
+        }
+    }
+
+    private void SetInteractable(IInteractable newInteractable)
+    {
+        if (newInteractable != currentInteractable)
+        {
+            currentInteractable = newInteractable;
+
+            canvasManager.ShowInteractionPanel(currentInteractable.GetInteractionText());
+        }
+    }
+
+    private void ClearInteractable()
+    {
+        currentInteractable = null;
+
+        canvasManager.HideInteractionPanel();
+    }
+}
