351c1b410a44a42e78673ebcfc0c2383ff09834f
Rui Martins <vandroste@gmail.com>
2018-12-15 00:23:01 +0000

    Add main menu prototype

---
+++ Assets/MainMenuObjects/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
